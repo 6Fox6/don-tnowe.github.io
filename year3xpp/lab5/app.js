f0364eb46dbdb7c8d0c14a159abd0f35587c2f69
don-tnowe <67479453+don-tnowe@users.noreply.github.com>
2021-11-04 19:50:25 +0200

    update lab5(task 1 finished)

--- year3xpp/lab5/app.js
+++ year3xpp/lab5/app.js
@@ -5,9 +5,28 @@ const app = express();
 app.get("/", function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
 
-    let params = request.url.substring(2).split("&");
+    let params = request.url.substring(request.url.lastIndexOf("/?") + 2).split("&");
 
+    console.log(params);
+    if (params.length == 0) {
+        let contents = fs.readFileSync("./index.html");
+        response.end(contents);
+        return;
+    }
     if (params[0] == "task=1") {
+        if (params.length == 1) {
+            let contents = fs.readFileSync("./students.html").toString();
+            response.end(contents.replace("%groups%", returnArr = fs.readdirSync("./students").reduce(
+                (prev, cur) => {
+                    if (prev != "")
+                        prev += ", ";
+
+                    return prev + cur.substring(0, cur.length - 4);
+                },
+                "")
+            ));
+            return;
+        }
         let group = params[1].substring("group=".length);
         if (!fs.existsSync("./students/" + group + ".txt")) {
             response.end("Групи " + group + " не існує!");
